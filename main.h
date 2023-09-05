b726a62d682b62b9e8020ca689cc55f0ae2537e1
Starceus001 <112934298+Starceus001@users.noreply.github.com>
2023-09-05 14:11:23 +0000

    Created base files for startup project

---
+++ main.h
@@ -0,0 +1,9 @@
+// ------------------------------------------
+// This file is property of:
+// Dennis Hiemstra - dennis.hiemstra@student.nhlstenden.com
+// Brent Nieuwland - brent.nieuwland@student.nhlstenden.com
+// Guido Annema - guido.annema@student.nhlstenden.com
+// 
+// Opdrachtgever - NHL Stenden Hogeschool, Leeuwarden
+// ------------------------------------------
+
